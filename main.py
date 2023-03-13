3359b09968580fdfb6a3994f61f5bb9bd00f3213
liuliu <liuliugit@gmail.com>
2023-03-13 19:38:32 +0800

    feat: refactor code

--- main.py
+++ main.py
@@ -22,13 +22,30 @@ def completion_for_model(model, prompt):
 
   return encode(encoding, prompt)
 
-def calculate_from_csv():
+def rows_from_csv():
   path = input('Input your CSV file path:') or 'demo.csv'
   if not (os.path.exists(path) and os.path.isfile(path)):
     raise Exception('Filepath not exists.')
 
   df = pandas.read_csv(path)
   return df.iterrows()
+
+def process_token_amount_to(rows, type, is_model = False):
+  result = {'Prompt': [], 'Amount Of Tokens': []}
+  for _, row in rows:
+    prompt = str(row['Prompt'])
+
+    if len(prompt) == 0:
+      continue
+
+    result['Prompt'].append(prompt)
+
+    if is_model:
+      result['Amount Of Tokens'].append(completion_for_model(type, prompt))
+    else:
+      result['Amount Of Tokens'].append(completion_for_encoding(type, prompt))
+
+  return result
 
 def write_to_csv(result):
   pandas.DataFrame(data=result).to_csv('demo_result.csv')
@@ -37,7 +54,6 @@ if __name__ == '__main__':
   types = ['1. model', '2. encoding']
   print('Which type of tokens calculator you want to use?')
   type_index = TerminalMenu(types).show()
-  result = {'Prompt': [], 'Amount Of Tokens': []}
 
   print(types[type_index])
 
@@ -45,7 +61,7 @@ if __name__ == '__main__':
 
   if type_index == 0:
     print('Which model you want to use?')
-    model_types = [
+    MODEL_TYPES = [
       'gpt-3.5-turbo-0301',
       'gpt-3.5-turbo',
       'text-davinci-003',
@@ -65,24 +81,19 @@ if __name__ == '__main__':
       'code-cushman-001'
     ]
 
-    type_index = TerminalMenu(model_types).show()
+    type_index = TerminalMenu(MODEL_TYPES).show()
 
-    print(model_types[type_index])
+    print(MODEL_TYPES[type_index])
 
-    for index, row in calculate_from_csv():
-      result['Prompt'].append(row['Prompt'])
-      result['Amount Of Tokens'].append(completion_for_model(model_types[type_index], row['Prompt']))
+    result = process_token_amount_to(rows_from_csv(), type=MODEL_TYPES[type_index], is_model=True)
     
   elif type_index == 1:
     print('Which encoding method you want to use?')
-    encoding_types = ['cl100k_base', 'p50k_base', 'r50k_base', 'gpt2']
-    type_index = TerminalMenu(encoding_types).show()
+    ENCODING_TYPES = ['cl100k_base', 'p50k_base', 'r50k_base', 'gpt2']
+    type_index = TerminalMenu(ENCODING_TYPES).show()
 
-    print(encoding_types[type_index])
+    print(ENCODING_TYPES[type_index])
 
-    for index, row in calculate_from_csv():
-      result['Prompt'].append(row['Prompt'])
-      result['Amount Of Tokens'].append(completion_for_encoding(encoding_types[type_index], row['Prompt']))
+    result = process_token_amount_to(rows_from_csv(), type=ENCODING_TYPES[type_index], is_model=False)
 
   write_to_csv(result)
-
